d7984f0dc08c6a1771b65c1c0ba1136d836e6b5c
sumnima-limbu <sumnimaklimbu@gmail.com>
2024-01-09 22:08:25 +0545

    Add about and team pages

---
+++ app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const metadata = {
+  title: "About Traversy Media",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi nisi
+        commodi qui dignissimos veritatis, consequatur earum pariatur
+        perferendis dolor accusamus omnis obcaecati quibusdam expedita incidunt
+        iure ipsum, asperiores impedit? Nesciunt! Lorem ipsum dolor sit, amet
+        consectetur adipisicing elit. Excepturi nisi commodi qui dignissimos
+        veritatis, consequatur earum pariatur perferendis dolor accusamus omnis
+        obcaecati quibusdam expedita incidunt iure ipsum, asperiores impedit?
+        Nesciunt!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste culpa
+        porro, minima incidunt perspiciatis commodi nesciunt distinctio, autem
+        cupiditate neque reprehenderit blanditiis? Sit eaque quidem
+        reprehenderit mollitia ducimus minus facilis. Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Praesentium qui saepe vitae unde
+        exercitationem delectus voluptatibus cum illum, cumque laudantium id
+        obcaecati ea sed officia cupiditate voluptatum vero culpa soluta!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
